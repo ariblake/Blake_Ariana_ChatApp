6f1e4084dcb97b17961595b2c8e9eb7ddf606b4a
Ariana Blake <a_blake69703@fanshaweonline.ca>
2020-01-29 08:43:33 -0500

    adding socket stuff

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,1 +1,18 @@
 // imports always go first - if we're importing anything
+
+// this is getting loaded in from our node_modules folder - make a socket variable and instantiate it here
+const socket = io();
+
+// the packet is whatever data we senf through with the connect event from the server
+function setUserId(packet) {
+    //debugger;
+    console.log(packet);
+}
+
+function showDisconnectMessage() {
+    console.log('a user disconnected');
+}
+
+
+socket.addEventListener('connected', setUserId);
+socket.addEventListener('disconnect', showDisconnectMessage);
