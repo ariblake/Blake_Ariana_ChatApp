79fd88501d3a9f6a089d100f841148842bbe8875
Ariana Blake <a_blake69703@fanshaweonline.ca>
2020-01-29 10:34:34 -0500

    added UI enhancements

--- public/js/modules/ChatMessage.js
+++ public/js/modules/ChatMessage.js
@@ -5,15 +5,16 @@ export default {
     props: ['msg'],
 
     template: `
-        <li>
-            <p class="new-message">
-                <span>{{ msg.message.name }} says: </span>
-                {{ msg.message.content }}
-            </p>
-        </li>
+        <p class="new-message" :class="{ 'my-message' : matchedID}">
+            <span>{{ msg.message.name }} says: </span>
+            {{ msg.message.content }}
+        </p>
     `,
 
     data: function() {
-        return { message: "hello from the template"};
+        return { 
+            message: "hello from the template",
+            matchedID: this.$parent.socketID == this.msg.id // parent is referring to main_vm.js
+        };
     }
 }
