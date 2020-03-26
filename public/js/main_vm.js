ce38fbbf5b29d6c417da598c58879b720861aff5
Ariana Blake <a_blake69703@fanshaweonline.ca>
2020-03-26 19:43:16 -0400

    styling changes

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -4,6 +4,14 @@ import ChatMessage from "./modules/ChatMessage.js";
 // this is getting loaded in from our node_modules folder - make a socket variable and instantiate it here
 const socket = io();
 
+// // append text if someone is online
+// socket.on('is_online', function(username) {
+//     $('#messages').append($('<li>').html(username));
+// });
+// // ask username
+// var username = prompt('Please tell me your name');
+// socket.emit('username', username);
+
 // the packet is whatever data we senf through with the connect event from the server
 // referring to the object key from app.js, just choosing sID rather than everything - data destructuring (look up on MDN)
 function setUserId({sID}) {
@@ -14,6 +22,7 @@ function setUserId({sID}) {
 
 function showDisconnectMessage() {
     console.log('a user disconnected');
+    io.emit('a user has disconnected');
 }
 
 function appendMessage(message) {
@@ -25,6 +34,7 @@ const vm = new Vue({
         socketID: "",
         message: "", // store whatever we type in the text area
         nickname: "",
+        //typing: false,
         messages: [
             // {
             //     name: "Ariana",
